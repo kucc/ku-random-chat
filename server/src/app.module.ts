6fe53148f508e81cfda4763436585cce0bbf3714
namda-on <jinyoung1997@gmail.com>
2021-04-05 02:13:14 +0900

    feat : init mongoose

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ChatModule } from './chat/chat.module';
+import { ReportModule } from './report/report.module';
+import { RoomModule } from './room/room.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_CONFIG),
+    ChatModule,
+    ReportModule,
+    RoomModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
