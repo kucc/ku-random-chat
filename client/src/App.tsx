af9d9e0483ea29ca89bdc9ed560a38095e12ac63
Jisu Lim <limjisuu@korea.ac.kr>
2021-04-10 02:57:04 +0900

    feat : add sign-in page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Home from "@pages/home";
+import SignIn from "./pages/sign-in";
 
 const App = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home}></Route>
+      <Route path="/sign-in" exact component={SignIn}></Route>
       <Redirect from="*" to="/" />
     </Switch>
   );
