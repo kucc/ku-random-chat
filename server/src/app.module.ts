1524d207837135bc22b30e58cad3a9052ad966ad
bangvkf3@gmail.com <jhbang@bangjunhyeog-ui-MacBookAir.local>
2021-04-28 21:17:27 +0900

    chore : 주석 제거

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -10,12 +10,12 @@ import { ConfigModule } from '@nestjs/config';
 import { AppGateway } from './app.gateway';
 @Module({
   imports: [
-    // ConfigModule.forRoot({ envFilePath: '.env' }),
-    // MongooseModule.forRoot(process.env.MONGODB_CONNECTION_CONFIG),
-    // ChatModule,
-    // ReportModule,
-    // RoomModule,
-    // UserModule,
+    ConfigModule.forRoot({ envFilePath: '.env' }),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_CONFIG),
+    ChatModule,
+    ReportModule,
+    RoomModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppGateway],
