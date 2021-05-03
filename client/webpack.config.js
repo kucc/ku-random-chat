d8baa491580aa5b9d0eb82451f697ffe78280f27
Jisu Lim <limjisuu@korea.ac.kr>
2021-05-04 00:17:44 +0900

    chore : install @babel/polyfill for async await

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.tsx",
+  entry: ["@babel/polyfill", "./src/index.tsx"],
   devtool: "eval-source-map",
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
