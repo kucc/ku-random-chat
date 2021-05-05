a2ef6e61bb093bc555bc2aee33d8a68cedd4a530
Jisu Lim <limjisuu@korea.ac.kr>
2021-05-05 09:01:08 +0900

    feat : add cors setting for server

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,10 @@ import * as mongoose from 'mongoose';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:8080',
+    credentials: true,
+  });
   await app.listen(3000);
   mongoose.set('debug', true);
 }
