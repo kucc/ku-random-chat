b03c1960788c1f9be994355b53c7ea1dd981e8d7
namda-on <jinyoung1997@gmail.com>
2021-05-10 02:56:19 +0900

    feat : 새로운 채팅방 생성 api
    임시 api

--- server/src/room/room.service.ts
+++ server/src/room/room.service.ts
@@ -2,10 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { RoomModel } from 'src/common/model/room.model';
-
+import Room from './model/room';
 @Injectable()
 export class RoomService {
+  chatRoomList: Room[];
   constructor(
     @InjectModel('Room') private readonly roomModel: Model<RoomModel>,
-  ) {}
+  ) {
+    this.chatRoomList = [];
+  }
+
+  createNewChatRoom(title: string) {
+    const newRoom = {} as Room;
+    newRoom.roomId = this.chatRoomList.length + 1;
+    newRoom.roomTitle = title;
+    this.chatRoomList.push(newRoom);
+
+    return newRoom;
+  }
 }
