57842a9f0518459fe30529dbf96b5a7d4fff137e
namda-on <jinyoung1997@gmail.com>
2021-05-09 20:26:19 +0900

    feat : add redirect logic at sign-in, sign-up

--- client/src/common/lib/axios.ts
+++ client/src/common/lib/axios.ts
@@ -1,14 +1,13 @@
-import axios from "axios";
-import endpoints from "../endpoints";
+import axios from 'axios';
+import endpoints from '../endpoints';
 
 const instance = axios.create({
   baseURL: endpoints.API_BASE_URL,
   withCredentials: true,
   headers: {
-    "Access-Control-Allow-Origin": true,
+    'Access-Control-Allow-Origin': true,
   },
 });
-
 instance.interceptors.response.use(
   (response) => Promise.resolve(response),
   (error) => Promise.reject(error)
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,31 +1,31 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: ["@babel/polyfill", "./src/index.tsx"],
-  devtool: "eval-source-map",
+  entry: ['@babel/polyfill', './src/index.tsx'],
+  devtool: 'cheap-module-source-map',
   resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
-      "@": path.resolve(__dirname, "src/"),
-      "@assets": path.resolve(__dirname, "src/assets/"),
-      "@components": path.resolve(__dirname, "src/components/"),
-      "@pages": path.resolve(__dirname, "src/pages"),
+      '@': path.resolve(__dirname, 'src/'),
+      '@assets': path.resolve(__dirname, 'src/assets/'),
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
     },
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      filename: "index.html",
+      template: './public/index.html',
+      filename: 'index.html',
     }),
   ],
   devServer: {
