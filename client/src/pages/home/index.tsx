a7218beff6c057000f254d660af29fb408bdce33
ju-sung-kang <jusung.dev@gmail.com>
2021-05-12 20:38:19 +0900

    refactor : require 말고 import 로 이미지 받아오기

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import * as S from "./styles";
 import Header from "@components/header";
+import realzz from "@assets/front.png";
 
 
 const Home = () => {
   return (
       <S.HomeContainer>
         <Header/>
-        <S.ImageContainer src = {require("@assets/front.png").default}/>
+        <S.ImageContainer src = {realzz}/>
         <S.ButtonContainer>랜덤매칭 입장하기</S.ButtonContainer>
         <S.ButtonContainer>방채팅</S.ButtonContainer>
         <S.Copyright>ⓒ KU RANDOM CHAT All rights reserved.</S.Copyright>
