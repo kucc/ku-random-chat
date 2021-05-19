151807066a751838e9e1525a1ef74277ef7cef5c
JunHyeok.Bang <bangvkf3@gmail.com>
2021-05-19 18:17:43 +0900

    feat : implement function for saving a new message in DB

--- server/src/chat/chat.service.ts
+++ server/src/chat/chat.service.ts
@@ -2,10 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { ChatModel } from '../common/model/chat.model';
+import { CreateMessageDTO } from './dto/createMessageDTO.dto';
 
 @Injectable()
 export class ChatService {
   constructor(
     @InjectModel('Chat') private readonly chatModel: Model<ChatModel>,
   ) {}
+
+  async createNewMessage(createMessageDTO: CreateMessageDTO) {
+    console.log(createMessageDTO);
+    const newMessage = new this.chatModel(createMessageDTO);
+    await newMessage.save();
+  }
 }
