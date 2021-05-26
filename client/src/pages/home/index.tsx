bf4af74154caff0bc0aa29db989c84dc7b6022d6
JunHyeok.Bang <bangvkf3@gmail.com>
2021-05-26 17:27:50 +0900

    fead : add link to buttons of home page

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,20 +1,33 @@
-import React from "react";
-import * as S from "./styles";
-import Header from "@components/header";
-import realzz from "@assets/front.png";
-
+import React from 'react';
+import * as S from './styles';
+import Header from '@components/header';
+import realzz from '@assets/front.png';
+import { useHistory } from 'react-router';
 
 const Home = () => {
+  const history = useHistory();
   return (
-      <S.HomeContainer>
-        <S.HomeCarrier>
-          <Header/>
-          <S.ImageContainer src = {realzz}/>
-          <S.ButtonContainer>랜덤매칭 입장하기</S.ButtonContainer>
-          <S.ButtonContainer>방채팅</S.ButtonContainer>
-          <S.Copyright>ⓒ KU RANDOM CHAT All rights reserved.</S.Copyright>
-        </S.HomeCarrier>
-      </S.HomeContainer>
+    <S.HomeContainer>
+      <S.HomeCarrier>
+        <Header />
+        <S.ImageContainer src={realzz} />
+        <S.ButtonContainer
+          onClick={() => {
+            history.push('/random-chat');
+          }}
+        >
+          랜덤매칭 입장하기
+        </S.ButtonContainer>
+        <S.ButtonContainer
+          onClick={() => {
+            history.push('/match-room');
+          }}
+        >
+          방채팅
+        </S.ButtonContainer>
+        <S.Copyright>ⓒ KU RANDOM CHAT All rights reserved.</S.Copyright>
+      </S.HomeCarrier>
+    </S.HomeContainer>
   );
 };
 
